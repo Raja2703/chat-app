e2f2419b9c1a532b8dff7d908a3a7d8c6fc9d7fb
Raja <srajasep27@gmail.com>
2024-01-09 19:59:22 +0530

    chat app is functional

--- client/src/App.js
+++ client/src/App.js
@@ -8,26 +8,51 @@ const socket = io.connect('http://localhost:3001');
 function App() {
 	const [name, setName] = useState('');
 	const [room, setRoom] = useState('');
+	const [joinedRoom, setJoinedRoom] = useState(false);
 
 	const joinRoom = () => {
 		if (name.trim() !== '' && room.trim() !== '') {
 			socket.emit('join_room', room);
+			setJoinedRoom(true);
 		}
 	};
 
-	return (
-		<div className="w-screen flex flex-col justify-center items-center">
-			<div>
-				<h3 className="text-center">Join a chat</h3>
-				<input type="text" placeholder="john" className="border border-gray-600 mr-2 px-2 rounded-sm" onChange={(e) => setName(e.target.value)} />
-				<input type="text" placeholder="room no" className="border border-gray-600 mr-2 px-2 rounded-sm" onChange={(e) => setRoom(e.target.value)} />
-				<button className="border border-gray-600 mr-2 px-2 rounded-sm" onClick={joinRoom}>
-					join
-				</button>
+	if (joinedRoom) {
+		return (
+			<div className="w-screen flex flex-col justify-center items-center">
+				<Chat socket={socket} name={name} roomId={room} />
 			</div>
-			<Chat socket={socket} name={name} roomId={room} />
-		</div>
-	);
+		);
+	} else {
+		return (
+			<div className="w-screen h-screen flex flex-col justify-center items-center">
+				<div class="area flex items-center justify-center">
+					<ul class="z-0 circles">
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+					<div className="z-10 border border-indigo-900 h-1/6 flex flex-col justify-center px-10 py-20 rounded-md">
+						<h3 className="text-center text-xl text-white">Join a chat</h3>
+						<div className="mb-4 mt-2">
+							<input type="text" placeholder="john" className="border outline-none border-gray-600 mr-2 px-2 py-1 rounded-sm" onChange={(e) => setName(e.target.value)} />
+							<input type="text" placeholder="room no" className="border outline-none border-gray-600 mr-2 px-2 py-1 rounded-sm" onChange={(e) => setRoom(e.target.value)} />
+						</div>
+						<button className="mr-2 px-6 py-1 bg-blue-600 text-white rounded-sm w-max self-center" onClick={joinRoom}>
+							join
+						</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
+import './index.css';
 
 const Chat = ({ socket, name, roomId }) => {
 	const [currentMessage, setCurrentMessage] = useState('');
+	const [messages, setMessages] = useState([
+		// { roomId: 1, author: 'raja', message: "Hello i'm raja", time: '11:12' },
+		// { roomId: 1, author: 'dhanush', message: "Hello i'm raja,I'm from avadi and currently living in pondy", time: '11:22' },
+	]);
 
 	const sendMessage = async () => {
 		if (currentMessage.trim() !== '') {
@@ -13,28 +19,69 @@ const Chat = ({ socket, name, roomId }) => {
 			};
 
 			await socket.emit('send_msg', messageData);
+			setMessages((current) => [...current, messageData]);
+			setCurrentMessage('');
 		}
 	};
 
 	useEffect(() => {
+		socket.off('receive_msg');
 		socket.on('receive_msg', (data) => {
-			console.log(data.message);
+			setMessages((current) => [...current, data]);
 		});
 	}, [socket]);
+
+	const otherPerson = 'ml-4 mt-2 w-max max-w-sm px-4 py-1 rounded-t-xl rounded-br-xl bg-gradient-to-r from-amber-400 to-orange-600 text-white';
+	const you = 'mr-4 mt-2 self-end w-max max-w-sm px-4 py-1 rounded-t-xl rounded-bl-xl bg-gradient-to-r from-blue-400 to-blue-600 text-white';
+
 	return (
-		<div>
-			{/* header */}
-			<div>
-				<p>Live Chat</p>
-			</div>
-			{/* body */}
-			<div></div>
-			{/* footer */}
-			<div className="mt-3">
-				<input type="text" className="border border-gray-600 mr-2 px-2 rounded-sm" onChange={(e) => setCurrentMessage(e.target.value)} />
-				<button className="border border-gray-600 mr-2 px-2 rounded-sm" onClick={sendMessage}>
-					&#9658;
-				</button>
+		<div class="area">
+			<ul class="circles">
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+			<div className="w-screen flex flex-col h-screen items-center backdrop-blur-sm">
+				{/* header */}
+				<div>
+					<p className="text-center text-xl mt-5 mb-2 text-white">Live Chat</p>
+				</div>
+				{/* body */}
+				<div className="border border-indigo-700 shadow-2xl h-5/6 w-4/6 rounded-lg pl-2">
+					<div className="h-5/6">
+						<div className="flex flex-col h-full w-full overflow-y-auto py-2">
+							{messages.map((data, i) => {
+								return (
+									<li key={i} className={data.author === name ? you : otherPerson}>
+										<p>{data.message}</p>
+										<p className="text-gray-300 text-end text-xs">{data.time}</p>
+									</li>
+								);
+							})}
+						</div>
+					</div>
+
+					{/* send message button */}
+					<div className="h-1/6 flex items-center justify-center">
+						<input
+							type="text"
+							value={currentMessage}
+							className="w-10/12 h-9 outline-none mr-2 px-2 rounded-sm"
+							onChange={(e) => setCurrentMessage(e.target.value)}
+							onKeyDown={(e) => e.key === 'Enter' && sendMessage()}
+						/>
+						<button className="border bg-blue-500 text-white border-blue-600 mr-2 px-2 rounded-sm h-9" onClick={sendMessage}>
+							send
+						</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
